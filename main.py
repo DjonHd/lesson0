6d328a0bf5feae043cc7dc6a94927709b0de305f
DjonHd <djonohara@gmail.com>
2024-08-25 20:23:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print(12+89-1) #"0st program"
+print(9**0.5*5) #"1st program"
+print(9.99>9.98 and 1000!=1000.1) #"2st program"
+print((2*2+2) == (2*(2+2))) #"3st program"
+print(type(float('123.456'))) #"4st program"
+print(float('123.456'))
+print((float('123.456'))*10)
+print(int(1234.56)%10)
+
+
